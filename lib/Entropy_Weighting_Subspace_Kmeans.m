e7699b2a47f8c3716a3c3af2ed8ba1dc2e9df57c
YixuanXu <yixuan_xu@outlook.com>
2018-01-10 22:31:29 +0800

    Jan 10, 2018

--- lib/Entropy_Weighting_Subspace_Kmeans.m
+++ lib/Entropy_Weighting_Subspace_Kmeans.m
@@ -1,4 +1,4 @@
-function [centroid, dimension_weight, class] = Subspace_Kmeans(data, iteration, K, beta, verbose)
+function [centroid, dimension_weight, class] = Entropy_Weighting_Subspace_Kmeans(data, iteration, K, beta, lambda, verbose)
 
 % Pre-allocate weights
 centroid = zeros(K,size(data,2)); % centroid for each class
@@ -21,7 +21,7 @@ for i = 1:iteration
     % body part
     class = classify(centroid,dimension_weight,data);
     centroid = centroid_update(centroid,data,class);
-    [dimension_weight,J] = dimension_weight_update(K,centroid,class,dimension_weight,data,beta);
+    [dimension_weight,J] = dimension_weight_update(K,centroid,class,dimension_weight,data,beta,lambda);
     
     % whether display intermediate information
     if(verbose == 1)
@@ -38,48 +38,20 @@ end
 
 % Details on the scheme of updating dimension weights
 function [alpha,J] = dimension_weight_update(K,m,c,alpha,data,beta)
-all_matrix = zeros(size(data,1),size(data,2));
-for i = 1:size(data,1)
-    all_matrix(i,:) = (m(c(i,1),:)-data(i,:)) .* (m(c(i,1),:)-data(i,:));
-end
-J = 0;
 
-for i = 1:K
-    class_matrix = all_matrix(c==i,:); % pick out data in all_matrix that corresponds to class i
-    t_sum = sum(class_matrix,1) + 1e-9; % adding a small positive constant to make weights computatable
-    J = J + (alpha(i,:).^beta) * t_sum';
-    if(size(class_matrix,1)>0)
-        for j = 1:size(alpha,2)  
-            % variables used to avoid redundant calculation
-            tt_numerator = t_sum(1,j);
-            t_denominator = sum(tt_numerator./t_sum,2);
-            alpha(i,j) = 1/((t_denominator+1e-9)^(1/(beta-1)));
-        end
-    else
-        continue;
-    end
-end
-
-% Normalize
-for i = 1:size(alpha,1)
-    alpha(i,:) = alpha(i,:)./(sum(alpha(i,:),2));
-end
-
+  % Under writting...
+  
 end
 
 function [result] = classify(m,alpha,data)
-
 result = zeros(size(data,1),1);
-
 % Construct temporary matrix for efficiently computing dimention-weighted distance
 matrix = zeros(size(m,1),size(data,2));
-
 for i = 1:size(result,1)
     for j = 1:size(m,1)
         matrix(j,:) = (m(j,:)-data(i,:)) .* (m(j,:)-data(i,:));
     end
     temp = sum(alpha .* matrix,2);
-    
     % To avoid more than one class having the minimum distance.
     t_index = find(temp == min(temp));
     result(i,1) = t_index(1,1);
