6e39b3b6e736890a728363e23af77ecd4b42d75e
YixuanXu <yixuan_xu@outlook.com>
2018-03-25 19:05:14 +0800

    Add tools

---
+++ tool/GenerateDataset.m
@@ -0,0 +1,16 @@
+function data = GenerateDataset(n, p, k, pd, varargin)
+% Generate synthetic dataset for further use.
+%
+% Args:
+%   n: the number of samples in each class (scalar or (k * 1))
+%   p: the number of features (scalar)
+%   k: the number of classes (scalar)
+%   pd: probability distribution of generated data (string)
+%   varargin: parameters on p 
+%
+% Returns:
+%   data: generated dataset
+
+
+end
+
---
+++ tool/PlotData.m
@@ -0,0 +1,52 @@
+function DataScatter(data, label, varargin)
+% Plot data, with different classes correspond to different colors.
+%
+% Args:
+%   data: original data (n * p)
+%   label: clustering result (n * 1), -1 in label corresponds to noisy data
+%   varargin{1,1}: clustering centroid (k * p), k the number of classes
+
+isc = 0; % whether plot centroid
+k = max(label); % the number of classes
+p = size(data ,2); % the number of features
+
+if(~isempty(varargin))
+    isc = 1;
+    centroid = varargin{1, 1};
+end
+
+if((p > 3) || (p < 1))
+    error('Unable to plot data exceeds 3-deminsion');
+end
+
+% Plot data
+if(p == 2)
+    for i = 1 : k
+        scatter(data(label==i,1), data(label==i,2), 'filled', 'DisplayName', strcat('Class-',num2str(i)));
+        hold on;
+    end
+elseif(p == 3)
+    for i = 1 : k
+        scatter3(data(label==i,1), data(label==i,2), data(label==i,3), 'filled', 'DisplayName', strcat('Class-',num2str(i)));
+        hold on;
+    end
+end
+
+% Plot centroid
+if(isc == 1)
+    if(p == 2)
+        scatter(centroid(:,1), centroid(:,2), 150, 'd', 'filled', 'DisplayName', 'Centroid');
+    elseif(p == 3)
+        scatter3(centroid(:,1), centroid(:,2), centroid(:,3), 150, 'd', 'filled', 'DisplayName', 'Centroid');
+    end
+end
+
+% Add legend if appropriate
+if(k < 10)
+    legend('show');
+end
+
+% Other settings
+grid on;
+end
+
