f1a0d315c9ebd668dbd02d34497af034e51b62d2
Yixuan Xu <yixuan_xu@outlook.com>
2020-10-28 19:21:00 +0800

    [ENH] Update the entrance file

--- Clustering.m
+++ Clustering.m
@@ -11,19 +11,19 @@ function [centroid, result] = Clustering(data, method, varargin)
 addpath('.\lib');
 addpath('.\tool');
 
-% Check input validity
+% Validate input parameters
 if((strcmp(method,'kmeans') || strcmp(method,'kmeans++')) && (size(varargin,2) ~= 2))
     error('The value of K should be predefined when using k-means and k-means++.');
 elseif((strcmp(method,'isodata') || strcmp(method,'ISODATA')) && (size(varargin,2) < 5))
-    error('Not enough input for ISODATA. Please use help for more information.');
+    error('Not enough input arguments for ISODATA. Please use help for more information.');
 elseif((strcmp(method,'mean_shift') || strcmp(method,'Mean_Shift')) && (size(varargin,2) ~= 1))
-    error('Invalid number of input for mean shift.');
+    error('Invalid number of input arguments for mean shift.');
 elseif((strcmp(method,'dbscan') || strcmp(method,'DBSCAN')) && (size(varargin, 2) ~= 2))
-    error('Invalid number of input for dbscan.');
+    error('Invalid number of input arguments for dbscan.');
 elseif((strcmp(method,'GMM') || strcmp(method,'gmm')) && (size(varargin, 2) ~= 2))
-    error('Invalid number of input for Gaussian Mixture Model');
+    error('Invalid number of input arguments for Gaussian Mixture Model');
 elseif((strcmp(method,'LVQ') || strcmp(method,'lvq')) && (size(varargin, 2) ~= 5))
-    error('Invalid number of input for Learning Vector Quantization');
+    error('Invalid number of input arguments for Learning Vector Quantization');
 end
 
 % Method entries
@@ -41,43 +41,41 @@ elseif(strcmp(method,'kmeans++') || strcmp(method,'kmeanspp'))
     PlotData(data, result, centroid);
 % ISODATA
 elseif(strcmp(method,'ISODATA') || strcmp(method,'isodata'))
-    desired_k = varargin{1,1}; % desired number of classes
-    iteration = varargin{1,2}; % maximum iteration time
-    minimum_n = varargin{1,3}; % minimum number of samples in one class
-    maximum_variance = varargin{1,4}; % maximum allowed variance of samples in one class
-    minimum_d = varargin{1,5}; %  minimum distance between two classes
+    desired_k = varargin{1,1};  % desired number of classes
+    iteration = varargin{1,2};  % maximum iteration time
+    minimum_n = varargin{1,3};  % minimum number of samples in one class
+    maximum_variance = varargin{1,4};  % maximum allowed variance of samples in one class
+    minimum_d = varargin{1,5};  %  minimum distance between two classes
     [centroid, result] = ISODATA(data, iteration, desired_k, minimum_n, maximum_variance, minimum_d);
     PlotData(data, result, centroid);
 % Mean Shift
 elseif(strcmp(method,'mean_shift') || strcmp(method,'Mean_Shift'))
-    thr = varargin{1,1}; % distance threshold
+    thr = varargin{1,1};  % distance threshold
     [centroid, result] = Mean_Shift(data, thr);
     PlotData(data, result, centroid);
 % DBSCAN
 elseif(strcmp(method,'dbscan') || strcmp(method,'DBSCAN'))
-    epsilon = varargin{1,1}; % distance threshold for finding neighbors
-    minPts = varargin{1,2};% minimum required number of neighbor points for adding one core object
-    centroid = nan; % DBSCAN will not calculate centroid
+    epsilon = varargin{1,1};  % distance threshold for finding neighbors
+    minPts = varargin{1,2}; % minimum required number of neighbor points for adding one core object
+    centroid = nan;  % DBSCAN will not calculate centroid
     result = DBSCAN(data, epsilon, minPts);
     PlotData(data, result);
 % Gaussian Mixture Model
 elseif(strcmp(method,'GMM') || strcmp(method,'gmm'))
-    k = varargin{1,1}; % the number of Guassian components
-    iter = varargin{1,2}; % maximum number of iterations
-    [result, alpha, centroid, sigma] = Gaussian_Mixture(data, k, iter);
+    k = varargin{1,1};  % the number of Guassian components
+    iter = varargin{1,2};  % maximum number of iterations
+    [result, ~, centroid, ~] = Gaussian_Mixture(data, k, iter);
     PlotData(data, result, centroid);
 % Learning Vector Quantization
 elseif(strcmp(method,'LVQ') || strcmp(method,'lvq'))
-    q = varargin{1,1}; % the number of prototypes
-    neta = varargin{1,2}; % learning rate
-    x = varargin{1,3}; % training data
-    y = varargin{1,4}; % label of x
-    iter = varargin{1,5}; % maximum number of iterations
+    q = varargin{1,1};  % the number of prototypes
+    neta = varargin{1,2};  % learning rate
+    x = varargin{1,3};  % training data
+    y = varargin{1,4};  % label of x
+    iter = varargin{1,5};  % maximum number of iterations
     [centroid, result] = LVQ(data, q, neta, x, y, iter);
     PlotData(data, result, centroid);
 else
-    error('Unknown method!');
+    error('NotImplementedError!');
 end
 end
-
-
--- Subspace_Clustering.m
+++
@@ -1,7 +0,0 @@
-function [ output_args ] = Subspace_Clustering( input_args )
-%SUBSPACE_CLUSTERING 此处显示有关此函数的摘要
-%   此处显示详细说明
-
-
-end
-
